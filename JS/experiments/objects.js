cb54a0393deb3f7a163192e4f0dc12a276e6c21e
ccofres <43300768+ccofres@users.noreply.github.com>
2020-06-28 14:29:32 -0400

    modified:   JS/experiments/objects.js 	modified:   README.md

--- JS/experiments/objects.js
+++ JS/experiments/objects.js
@@ -30,26 +30,30 @@ function suggestMusic(person) {
       console.log("Uh, maybe try David Bowie? Everyone likes David Bowie, right?");
     }
 }
-
 suggestMusic(person1);
 suggestMusic(person2);
 suggestMusic(person);
 
 const dog = {
   name: "dog",
-  speak() {
-  console.log("woof woof");
+  speak() {                     // METODO
+    console.log("woof woof");
   }
 };
 dog.speak();
 
+
 const me = {
   name: {
-    first:`Cristian`,         //name es una key
-    last:`Cofre`
+    first:`Cristian`,            //name es una key
+    last:`Cofre`,
+    pollos:{
+      another: `another woof woof, by pollos`,
+      anotherPollos: "Arrrggghh, by pollos"
+    }
   },
   location:{
-    city: `The city of cold`,          //las key deben ser unicas
+    city: `The city of cold`,   //las key deben ser unicas
     state: `Araucania`,
     country: `Chile`
   }
@@ -58,6 +62,13 @@ console.log(me);                //Muestra toda la informacion
 console.log(me.name);           // Cristian Cofre
 console.log(me.name.first);     // Cristian
 console.log(me.location.country);  // The city of cold
-
-
+console.log(me.name.pollos);
+console.log(me.name.pollos.another);
+console.log(me.name.pollos.anotherPollos);
+/*
+ * No hay límite para objetos anidados (nested objects)
+ * No hay límite para la cantidad de propiedades que un objeto puede tener
+ * Las propiedades de un objeto pueden ser de cualquier tipo...
+ * Si una propiedad de un objeto es una funcion ==> es un METODO
+ */
 
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Frontend Master Bootcamp
 
 ### Web-site: https://frontendmasters.github.io/bootcamp/
+
+- https://jgthms.com/javascript-in-14-minutes/
+
+- https://eloquentjavascript.net/
+
+- https://javascript.info/
