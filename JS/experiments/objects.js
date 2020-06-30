2733b8a3803b7745f713e479c1b329f57f76d3ec
ccofres <43300768+ccofres@users.noreply.github.com>
2020-06-30 19:37:52 -0400

    modified:   JS/experiments/index.html 	modified:   JS/experiments/objects.js 	JS/experiments/array.js

--- JS/experiments/objects.js
+++ JS/experiments/objects.js
@@ -1,3 +1,5 @@
+/**
+
 const person = {
     name: `Cristian Cofre`,         //name es una key
     city: `The city of cold`,                //las key deben ser unicas
@@ -50,12 +52,23 @@ const me = {
     pollos:{
       another: `another woof woof, by pollos`,
       anotherPollos: "Arrrggghh, by pollos"
+    },
+    logMeOut(){
+      console.log(this);
     }
   },
   location:{
     city: `The city of cold`,   //las key deben ser unicas
+    streetNumber: 307,
+    street: `San Luis`,
     state: `Araucania`,
     country: `Chile`
+  },
+  getAddress(){
+    return `${me.name.first} ${this.name.last}
+${this.location.streetNumber} ${this.location.street}
+${this.location.city} ${this.location.state} 
+${this.location.country}`
   }
 };
 console.log(me);                //Muestra toda la informacion
@@ -65,10 +78,23 @@ console.log(me.location.country);  // The city of cold
 console.log(me.name.pollos);
 console.log(me.name.pollos.another);
 console.log(me.name.pollos.anotherPollos);
+console.log(me.getAddress());
+console.log(me.name.logMeOut());    //this en este caso es el objeto name
+me.name.first = `Baco`;
+console.log(me.name.logMeOut());
+
+*/
+
+console.log(this === window);
+console.log(this.screenY);
+console.log(window.screenY);
+
 /*
  * No hay límite para objetos anidados (nested objects)
  * No hay límite para la cantidad de propiedades que un objeto puede tener
  * Las propiedades de un objeto pueden ser de cualquier tipo...
  * Si una propiedad de un objeto es una funcion ==> es un METODO
+ * THIS.NAME.FIRST se refiere al objeto en donde "this" está siendo llamada
+ * En el caso anterior, this se refiere al objeto me
  */
 
